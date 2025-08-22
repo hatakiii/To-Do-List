89fd17acee682e64007a71d2c7903b139d0bb5ac
hatakiii <25lp5598@nest.edu.mn>
2025-08-22 17:03:17 +0800

    some changes

--- to-do-list-app/src/app/page.js
+++ to-do-list-app/src/app/page.js
@@ -1,7 +1,37 @@
-import Image from "next/image";
+"use client";
 
-function Home() {
-  return <div>To Do list1</div>;
-}
+import { useState } from "react";
 
-export default Home;
+const TodoPage = () => {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleOnchange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  // Zovhon darah uyd l ajilladag function bichne
+  const handleOnClick = () => {
+    setTodos([...todos, { title: inputValue, isDone: true }]);
+    setInputValue("");
+  };
+  return (
+    <div>
+      <input
+        value={inputValue}
+        type="text"
+        placeholder="add todo"
+        onChange={handleOnchange}
+      />
+      {todos.map((todo, index) => (
+        <div className="flex gap-10" key={index + Math.random()}>
+          <input type="checkbox" defaultChecked={todo.isDone} />
+          <div key={index}>{todo.title}</div>
+        </div>
+      ))}
+      <button onClick={handleOnClick}>add</button>
+    </div>
+  );
+};
+
+export default TodoPage;
