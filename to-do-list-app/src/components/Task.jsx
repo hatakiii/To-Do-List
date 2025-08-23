8ebe24cfc4f3dfeeed954d658affebf4d050fcfb
hatakiii <25lp5598@nest.edu.mn>
2025-08-23 12:33:33 +0800

    1234

---
+++ to-do-list-app/src/components/Task.jsx
@@ -0,0 +1,7 @@
+export const Task = (props) => {
+  return (
+    <div className="text-[14px] text-[#6b7280] text-center pt-[10px] pb-[38px] ">
+      No tasks yet. Add one above!
+    </div>
+  );
+};
