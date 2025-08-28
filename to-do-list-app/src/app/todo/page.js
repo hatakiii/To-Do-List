e40e072c35f29a7fdd8c85ce270302bfbc03df38
Khatanbaatar A <25lp5598@nest.edu.mn>
2025-08-28 10:19:33 +0800

    todo-g oorchilloo

--- to-do-list-app/src/app/todo/page.js
+++ to-do-list-app/src/app/todo/page.js
@@ -1,97 +1,143 @@
 "use client";
 
 import { useState } from "react";
+
+import { TaskCompleted, NoTask, Title, Footer } from "@/components";
+
 import { v4 as uuidv4 } from "uuid";
 
-const TodoPage = () => {
-  const [inputValue, setInputValue] = useState("");
+const Home = () => {
   const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState("");
   const [filterStatus, setFilterStatus] = useState("all");
 
   const handleFilterStatus = (status) => setFilterStatus(status);
 
-  const handleOnChange = (event) => setInputValue(event.target.value);
-
-  const handleAddTodo = () => {
-    if (!inputValue.trim()) return;
-    setTodos([...todos, { title: inputValue, isDone: false, id: uuidv4() }]);
-    setInputValue("");
+  const handleOnChange = (event) => {
+    setInputValue(event.target.value);
   };
 
-  const handleOnChangeCheckBox = (event, index) => {
-    const newTodos = todos.map((todo, i) =>
-      i === index ? { ...todo, isDone: event.target.checked } : todo
-    );
+  const handleOnClick = (event) => {
+    event.preventDefault();
+    if (!inputValue.trim()) return;
+    setTodos([
+      { title: inputValue, isCompleted: false, id: uuidv4() },
+      ...todos,
+    ]);
+    setInputValue("");
+    setFilterStatus("all");
+  };
+
+  const handleDelete = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const handleOnChangeChecked = (event, id) => {
+    // console.log(event.target.checked, id);
+    const newTodos = todos.map((el, i) => {
+      if (el.id === id) {
+        el.isCompleted = event.target.checked;
+      }
+      return el;
+    });
     setTodos(newTodos);
   };
+  const completedCount = todos.filter((todo) => todo.isCompleted).length;
 
-  const handleDeleteTodo = (index) => {
-    setTodos(todos.filter((_, i) => i !== index));
+  const handleClearCompleted = () => {
+    setTodos(todos.filter((todo) => !todo.isCompleted));
   };
 
   const filteredTodos = todos.filter((todo) => {
     if (filterStatus === "all") return true;
-    if (filterStatus === "active") return !todo.isDone;
-    return todo.isDone;
+    if (filterStatus === "active") return !todo.isCompleted;
+    return todo.isCompleted;
   });
 
+  // console.log(todos);
   return (
-    <div className="p-4">
-      {/* Input & Add button */}
-      <div className="flex gap-2 mb-4">
-        <input
-          value={inputValue}
-          onChange={handleOnChange}
-          type="text"
-          className="border p-2 rounded-md"
-        />
-        <button
-          onClick={handleAddTodo}
-          className="bg-green-500 text-white px-4 py-2 rounded-md"
-        >
-          add
-        </button>
-      </div>
+    <div className="m-0 p-0 box-border font-inter">
+      {/* Container */}
 
-      {/* Filters */}
-      <div className="flex gap-2 mb-4">
-        {["all", "active", "completed"].map((status) => (
-          <button
-            key={status}
-            onClick={() => handleFilterStatus(status)}
-            className={`bg-gray-500 text-white p-2 rounded-md ${
-              filterStatus === status ? "!bg-blue-500" : ""
-            }`}
-          >
-            {status}
-          </button>
-        ))}
-      </div>
-
-      {/* Todos */}
-      {filteredTodos.map((todo, index) => (
+      <div className="p-top pt-[60px] bg-[#f3f5f7] min-h-[100vh]">
+        {/* Inner Container */}
         <div
-          key={todo.id}
-          className="flex items-center gap-2 border-b border-gray-300 p-2 w-fit"
+          className="w-[377px]  pt-[24px] pb-[24px] pl-[16px] pr-[16px] rounded-[6px] mx-auto bg-[#fff] shadow-[0_0_12px_rgba(0,0,0,0.16)]
+"
         >
-          <input
-            type="checkbox"
-            checked={todo.isDone}
-            onChange={(event) => handleOnChangeCheckBox(event, index)}
-          />
-          <p className={todo.isDone ? "line-through text-gray-500" : ""}>
-            {todo.title}
-          </p>
-          <button
-            onClick={() => handleDeleteTodo(index)}
-            className="text-red-500"
-          >
-            delete
-          </button>
+          {/* Title */}
+          <Title></Title>
+          {/* Form= Input and Button */}
+          <form className="h-[40px] flex gap-[6px]" onClick={handleOnClick}>
+            <input
+              value={inputValue}
+              type="text"
+              onChange={handleOnChange}
+              maxLength="100"
+              placeholder="Add a new task..."
+              className="w-72 h-10 px-4 py-2 rounded-md outline-1 outline-offset-[-1px] outline-border-border-border inline-flex flex-col justify-center items-start"
+            ></input>
+            <button
+              type="submit"
+              className="w-[59px] border-0 cursor-pointer text-[14px] text-[#f9f9f9] rounded-[6px] px-[16px] bg-[#3c82f6]  active:bg-blue-600 hover:bg-blue-700"
+            >
+              Add
+            </button>
+          </form>
+          {/* Filter Buttons */}
+          <div className="h-[32px] flex gap-[6px] mt-[20px] mb-[20px]">
+            {["all", "active", "completed"].map((status) => (
+              <button
+                key={status}
+                onClick={() => handleFilterStatus(status)}
+                className={
+                  "border-0 cursor-pointer text-neutral-700 text-xs font-normal rounded-[6px] px-[12px] capitalize bg-[#e5e7eb] " +
+                  `${
+                    filterStatus === status
+                      ? "!bg-[#3c82f6] !text-[#fff] hover:!bg-blue-700"
+                      : ""
+                  }`
+                }
+              >
+                {status}
+              </button>
+            ))}
+          </div>
+          {/* No tasks to display */}
+          {filteredTodos.length === 0 && <NoTask />}
+          {filteredTodos.map((todo) => (
+            <TaskCompleted
+              key={todo.id}
+              id={todo.id}
+              taskName={todo.title}
+              isCompleted={todo.isCompleted}
+              handleDelete={handleDelete}
+              handleOnChangeChecked={handleOnChangeChecked}
+            ></TaskCompleted>
+          ))}
+
+          {/* Clear completed */}
+          {filteredTodos.length !== 0 && (
+            <div className="flex pt-4 mb-10   justify-between items-center border-t-[1px] border-t-[#e5e7eb]">
+              <p className="text-[14px] text-gray-500 ">
+                {completedCount} of {todos.length} tasks completed
+              </p>
+              {completedCount > 0 && (
+                <button
+                  onClick={handleClearCompleted}
+                  className="border-0 text-[14px] cursor-pointer  text-[#ef4444] bg-transparent hover:bg-red-100 active:bg-red-200 rounded-md p-1"
+                >
+                  Clear completed
+                </button>
+              )}
+            </div>
+          )}
+
+          {/* Footer */}
+          <Footer />
         </div>
-      ))}
+      </div>
     </div>
   );
 };
-
-export default TodoPage;
+export default Home;
